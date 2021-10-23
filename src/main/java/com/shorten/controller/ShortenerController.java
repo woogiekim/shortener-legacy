a4bf164ecce7bc4448091a4d50c9e5df309110f1
wook <mdir2@naver.com>
2021-10-23 23:36:19 +0900

    refactor(shorten-response): shorten url response 스펙 변경 (필요없는 롬복 제거)

--- src/main/java/com/shorten/controller/ShortenerController.java
+++ src/main/java/com/shorten/controller/ShortenerController.java
@@ -1,11 +1,7 @@
 package com.shorten.controller;
 
+import com.shorten.domain.dto.ShortenedUrlResponse;
 import com.shorten.shortener.Base62Generator;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,23 +15,9 @@ public class ShortenerController {
     // generate shortened url
     final String shortenedUrl = Base62Generator.generateKey(url);
 
-    final ShortenedUrlResponse shortenedUrlResponse = ShortenedUrlResponse
-        .builder()
-        .url(url)
-        .shortenedUrl(shortenedUrl)
-        .build();
+    final ShortenedUrlResponse shortenedUrlResponse = ShortenedUrlResponse.of(url, shortenedUrl);
 
     return Response.ok(shortenedUrlResponse);
   }
 
-  @Data
-  @Builder
-  @NoArgsConstructor
-  @AllArgsConstructor
-  @EqualsAndHashCode
-  private static class ShortenedUrlResponse {
-
-    private String url;
-    private String shortenedUrl;
-  }
 }
