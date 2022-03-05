65f20e0d79312a23b821b838df4463c84264ef79
Woogie Kim <mdir2kor@gmail.com>
2022-03-06 02:12:36 +0900

    shortener-4 유효성검사 코드 spring Assert 로 변경

--- src/main/java/com/shorten/domain/entity/ShortenUrl.java
+++ src/main/java/com/shorten/domain/entity/ShortenUrl.java
@@ -1,8 +1,7 @@
 package com.shorten.domain.entity;
 
-import java.net.URI;
-import java.util.Objects;
 import lombok.EqualsAndHashCode;
+import org.springframework.util.Assert;
 
 @EqualsAndHashCode
 public class ShortenUrl {
@@ -11,7 +10,8 @@ public class ShortenUrl {
   private final ShortenPath shortenPath;
 
   private ShortenUrl(final RedirectUrl redirectUrl, final ShortenPath shortenPath) {
-    verifyShortenUrl(redirectUrl, shortenPath);
+    Assert.notNull(redirectUrl, "리다이렉트 주소 정보가 없습니다.");
+    Assert.notNull(shortenPath, "짧은 주소 패스 정보가 없습니다.");
 
     this.redirectUrl = redirectUrl;
     this.shortenPath = shortenPath;
@@ -21,21 +21,7 @@ public class ShortenUrl {
     return new ShortenUrl(redirectUrl, shortenPath);
   }
 
-  private static void verifyShortenUrl(final RedirectUrl redirectUrl, final ShortenPath shortenPath) {
-    if (Objects.isNull(redirectUrl)) {
-      throw new IllegalArgumentException("리다이렉트 주소 정보가 없습니다.");
-    }
-
-    if (Objects.isNull(shortenPath)) {
-      throw new IllegalArgumentException("짧은 주소 패스 정보가 없습니다.");
-    }
-  }
-
   public String toUriString() {
     return String.join("/", this.redirectUrl.toString(), this.shortenPath.toString());
-  }
-
-  public URI toRedirectUri() {
-    return this.redirectUrl.getUri();
   }
 }
